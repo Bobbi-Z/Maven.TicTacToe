66824d60b0a40ee96195dc6b8dc3dd3b1fa400d1
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-05 03:31:55 -0400

    favorofo passed getwinner attempt 1

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -46,6 +46,7 @@ public class Board {
         int counterEmpty = 0;
         char inFavorOfX = 'X';
         char inFavorOfO = 'O';
+        char inFavorOfEmpty = ' ';
 
         // [row][column]
         for (int indexRow = 0; indexRow < matrix.length; indexRow++) {
@@ -70,11 +71,89 @@ public class Board {
     }
 
     public Boolean isTie() {
+
         return null;
     }
 
     public String getWinner() {
-        return null;
+        int counterX = 0;
+        int counterO = 0;
+        int counterEmpty = 0;
+        char inFavorOfX = 'X';
+        char inFavorOfO = 'O';
+        char inFavorOfEmpty = ' ';
+        if (matrix[0][0] == matrix [0][1] && matrix[0][1] == matrix[0][2]) { //row 1 win
+                if (matrix[0][0].charValue() == inFavorOfX){
+                    counterX += 1;
+                } else if (matrix[0][0].charValue() == inFavorOfO){
+                    counterO += 1;
+                } else { counterEmpty += 1;
+                }
+            }else if (matrix[1][0] == matrix [1][1] && matrix[1][1] == matrix[1][2]) { //row 2 win
+            if (matrix[1][0].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[1][0].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[2][0] == matrix [2][1] && matrix[2][1] == matrix[2][2]) { //row 3 win
+            if (matrix[2][0].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[2][0].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[0][0] == matrix [1][0] && matrix[1][0] == matrix[2][0]) { //column 1 win
+            if (matrix[0][0].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[0][0].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[0][1] == matrix [1][1] && matrix[1][1] == matrix[2][1]) { //column 2 win
+            if (matrix[0][1].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[0][1].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[0][2] == matrix [1][2] && matrix[1][2] == matrix[2][2]) { //column 3 win
+            if (matrix[0][2].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[0][2].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[0][0] == matrix [1][1] && matrix[1][1] == matrix[2][2]) { //diagonal top to bottom win
+            if (matrix[0][0].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[0][0].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else if (matrix[2][0] == matrix [1][1] && matrix[1][1] == matrix[0][2]) { //diagonal bottom to top win
+            if (matrix[2][0].charValue() == inFavorOfX) {
+                counterX += 1;
+            } else if (matrix[2][0].charValue() == inFavorOfO) {
+                counterO += 1;
+            } else {
+                counterEmpty += 1;
+            }
+        }else{
+        }
+       if (counterX > counterO){
+           return String.valueOf(inFavorOfX);
+       }else if ( counterO > counterX){
+           return String.valueOf(inFavorOfO);
+       }else {
+           return String.valueOf(inFavorOfEmpty);
+       }
     }
 
 }
